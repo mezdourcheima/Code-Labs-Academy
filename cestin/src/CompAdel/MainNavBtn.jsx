ec2d3cd2ed43226f56114515cfc89125dfa6793b
Cheima_Mezdour <74424575+mezdourcheima@users.noreply.github.com>
2023-03-15 11:49:02 +0100

    Delete cestin directory

--- cestin/src/CompAdel/MainNavBtn.jsx
+++
@@ -1,9 +0,0 @@
-const MainNavBtn = ({text, color}) => {
-    return (
-      <button className={`w-full text-[#FFF] bg-[${color}] rounded`}>
-          {text}
-      </button>
-    )
-  }
-  
-  export default MainNavBtn
--- cestin/src/Components/Component2.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function MyComponentt(props) {
-    return <h1>Hello, {props.name}!</h1>;
-  }
-
-
- export default MyComponentt;
